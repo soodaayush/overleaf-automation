e43ee3ce485477efcf295f44ba9c50d699fc6a2b
Aayush <aayush.sood@icloud.com>
2026-07-21 11:49:42 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+from playwright.sync_api import sync_playwright
+from playwright_recaptcha import recaptchav2
+from dotenv import load_dotenv
+
+load_dotenv()
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+
+    context = browser.new_context()
+    page = context.new_page()
+
+    page.goto("https://www.overleaf.com/login")
+
+    print(f"Page Title: {page.title()}")
+
+    page.locator("input#email").fill(os.getenv("EMAIL"))
+    page.locator("input#password").fill(os.getenv("PASSWORD"))
+
+    page.locator("button.btn").first.click()
+
+    with recaptchav2.SyncSolver(page) as solver:
+        token = solver.solve_recaptcha(wait=True)
+
+        page.wait_for_load_state("networkidle")
+
+    page.get_by_text("UW Resume").click()
+    page.wait_for_timeout(2000)
+
+
